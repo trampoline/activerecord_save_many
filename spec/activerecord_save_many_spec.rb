156c2511d7596e7e840ab61195aa94d4cf565719
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2010-02-15 23:57:56 +0000

    set up rspec

--- spec/activerecord_save_many_spec.rb
+++ spec/activerecord_save_many_spec.rb
@@ -5,5 +5,11 @@ module ActiveRecord
     it "fails" do
       fail "hey buddy, you should probably rename this file and start specing for real"
     end
+
+    it "creates a save_many method on an ActiveRecord class" do
+      class Foo < ActiveRecord::Base ; end
+
+      Foo.respond_to?(:save_many).should be(true)
+    end
   end
 end
