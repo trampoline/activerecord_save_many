527d7847dfa3f13f1e58e95c7183e802533aa7ba
mccraig mccraig of the clan mccraig <mccraigmccraig@googlemail.com>
2010-02-16 13:45:08 +0000

    adding some specs

--- lib/activerecord_save_many.rb
+++ lib/activerecord_save_many.rb
@@ -2,15 +2,55 @@ require 'set'
 
 module ActiveRecord
   module SaveMany
+    MAX_QUERY_SIZE = 1024 * 1024
+    OPTIONS_KEYS = [:columns, :max_rows, :async, :ignore, :update, :updates].to_set
+
     class << self
       attr_accessor :default_max_rows
     end
     self.default_max_rows = 50000
 
-    module ClassMethods
-      MAX_QUERY_SIZE = 1024 * 1024
-      OPTIONS_KEYS = [:columns, :max_rows, :insert_delayed, :ignore, :update, :updates].to_set
+    module Functions
+      def rails_env
+        RAILS_ENV if defined? RAILS_ENV
+      end
+      module_function :rails_env
 
+      def disable_async?
+        # for predictable tests we disable delayed insert during testing
+        rails_env()=="test"
+      end
+      module_function :disable_async?
+
+      def check_options(options)
+        unknown_keys = options.keys.to_set - OPTIONS_KEYS.to_set
+        raise "unknown options: #{unknown_keys.to_a.join(", ")}" if !unknown_keys.empty?
+      end
+      module_function :check_options
+
+      # slice an array into smaller arrays with maximum size max_size
+      def slice_array(max_length, arr)
+        slices = []
+        (0..arr.length-1).step( max_length ){ |i| slices << arr.slice(i,max_length) }
+        slices
+      end
+      module_function :slice_array
+
+      def add_columns(klass, values, options)
+        columns = options[:columns] || klass.columns.map(&:name)
+
+        # add a :type column automatically for STI, if not already present
+        if klass.superclass!=ActiveRecord::Base && !columns.include?(:type)
+          columns = [:type, *columns]
+          values = values.map{|vals| [klass.to_s, *vals]}
+        end
+
+        [columns, values]
+      end
+      module_function :add_columns
+    end
+
+    module ClassMethods
       def save_many_max_rows=(max_rows)
         @save_many_max_rows=max_rows
       end
@@ -19,38 +59,15 @@ module ActiveRecord
         @save_many_max_rows || ActiveRecord::SaveMany::default_max_rows
       end
 
-      def disable_delayed_insert?
-        # for predictable tests we disable delayed insert during testing
-        RAILS_ENV=="test" if defined? RAILS_ENV
-      end
-
-      def check_options(options)
-        unknown_keys = options.keys.to_set - OPTIONS_KEYS.to_set
-        raise "unknown options: #{unknown_keys.join(", ")}" if !unknown_keys.empty?
-      end
-
-      # slice an array into smaller arrays with maximum size max_size
-      def slice_array(max_length, arr)
-        slices = []
-        (0..arr.length-1).step( max_length ){ |i| slices << values.slice(i,max_length) }
-        slices
-      end
-
-      def save_many(values, options={} )
-        check_options(options)
+      def save_many(values, options={})
+        Functions::check_options(options)
         return if values.nil? || values.empty?
 
-        columns = options[:columns] || self.columns.map(&:name)
-
-        # add a :type column automatically for STI, if not already present
-        if self.superclass!=ActiveRecord::Base && !columns.include?(:type)
-          columns = [:type, *columns]
-          values = values.map{|vals| [to_s, *vals]}
-        end
+        columns, values = Functions::add_columns(self, values, options)
 
         # if more than max_rows, execute multiple sql statements
         max_rows = options[:max_rows] || save_many_max_rows
-        batches = slice_array(max_rows, values)
+        batches = Functions::slice_array(max_rows, values)
 
         column_list = columns.join(', ')
         do_updates = options[:update] || options[:updates]
@@ -70,7 +87,7 @@ module ActiveRecord
             columns.map{|col| obj[col]}
           end
 
-          insert_stmt = options[:insert_delayed] && !disable_delayed_insert? ? "insert delayed" : "insert"
+          insert_stmt = options[:async] && !disable_async? ? "insert delayed" : "insert"
           ignore_opt = options[:ignore] ? "ignore" : ""
           
           sql = "#{insert_stmt} #{ignore_opt} into #{table_name} (#{column_list}) values " + 
--- spec/activerecord_save_many_spec.rb
+++ spec/activerecord_save_many_spec.rb
@@ -2,14 +2,103 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 module ActiveRecord
   describe SaveMany do
-    it "fails" do
-      fail "hey buddy, you should probably rename this file and start specing for real"
+    def new_anon_class(parent, &proc)
+      klass = Class.new(parent)  
+      klass.class_eval(&proc) if proc
+      klass
+    end
+
+    def new_named_class(parent, name="", &proc)
+      new_anon_class(parent) do
+        mc=self.instance_eval{ class << self ; self ; end }
+        mc.send(:define_method, :to_s){name}
+        self.class_eval(&proc) if proc
+      end
+    end
+
+    # create a new anonymous ActiveRecord::Base descendant class
+    def new_ar_class(name="", &proc)
+      new_named_class(ActiveRecord::Base, name, &proc)
+    end
+
+    describe SaveMany::Functions do
+      describe "disable_async?" do
+        it "should disable async inserts when testing" do
+          mock(SaveMany::Functions).rails_env{"test"}
+          SaveMany::Functions::disable_async?.should == true
+        end
+
+        it "should permit async inserts when not testing" do
+          mock(SaveMany::Functions).rails_env{"production"}
+          SaveMany::Functions::disable_async?.should == false
+        end
+      end
+
+      describe "check_options" do
+        it "should raise if given unknown options" do
+          lambda do
+            SaveMany::Functions::check_options(:foo=>100)
+          end.should raise_error(RuntimeError)
+        end
+      end
+
+      describe "slice_array" do
+        it "should slice arrays without losing bits" do
+          SaveMany::Functions::slice_array(2,[]).should ==([])
+          SaveMany::Functions::slice_array(2,[1]).should ==([[1]])
+          SaveMany::Functions::slice_array(2,[1,2]).should ==([[1,2]])
+          SaveMany::Functions::slice_array(2,[1,2,3]).should ==([[1,2],[3]])
+          SaveMany::Functions::slice_array(2,[1,2,3,4]).should ==([[1,2],[3,4]])
+        end
+      end
+
+      describe "add_columns" do
+        it "should add a type column to an indirect inheritor of ActiveRecord::Base" do
+          klass = new_named_class(new_ar_class("Foo"), "Bar")
+          columns, values = SaveMany::Functions::add_columns(klass, [["foo"]], :columns=>[:foo])
+          columns.should == [:type, :foo]
+          values.should == [["Bar", "foo"]]
+        end
+
+        it "should not add a type column if already present" do
+          klass = new_named_class(new_ar_class("Foo"), "Bar")
+          columns, values = SaveMany::Functions::add_columns(klass, [["foo", "Baz"]], :columns=>[:foo, :type])
+          columns.should == [:foo, :type]
+          values.should == [["foo", "Baz"]]
+        end
+
+        it "should not add a type column to a direct inheritor of ActiveRecord::Base" do
+          klass = new_ar_class("Foo")
+          columns, values = SaveMany::Functions::add_columns(klass, [["foo"]], :columns=>[:foo])
+          columns.should == [:foo]
+          values.should == [["foo"]]
+        end
+      end
     end
 
     it "creates a save_many method on an ActiveRecord class" do
-      class Foo < ActiveRecord::Base ; end
-
-      Foo.respond_to?(:save_many).should be(true)
+      klass = new_ar_class()
+      klass.respond_to?(:save_many).should be(true)
     end
+
+    it "should have per-class configurable save_many_max_rows" do
+      k1 = new_ar_class()
+      k2 = new_ar_class()
+      k1.save_many_max_rows=1000
+      k2.save_many_max_rows=2000
+
+      k1.save_many_max_rows.should == 1000
+      k2.save_many_max_rows.should == 2000
+    end
+
+    it "should have a global configurable default_max_rows" do
+      k1 = new_ar_class()
+      k2 = new_ar_class()
+      ActiveRecord::SaveMany::default_max_rows = 100
+      k1.save_many_max_rows.should == 100
+      k2.save_many_max_rows.should == 100
+    end
+
+    
   end
 end
